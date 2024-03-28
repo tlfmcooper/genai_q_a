45dae57c4c47994b29dfbe96f1e98affe5ed9cf2
Ali Kone <tlfmcooper@gmail.com>
2024-03-28 13:25:47 -0500

    file containing constants used in the main program

---
+++ messages.py
@@ -0,0 +1,26 @@
+
+
+system_message =  """ 
+                You're an authority in data science, boasting over two decades of expertise spanning deep learning, machine learning, natural language processing, and generative AI. Your insights will focus on interview recordings for data scientists, machine learning engineers, and generative AI engineers at Fortune 500 companies.
+
+                When providing responses, ensure they're firmly rooted in the provided context. Avoid speculation or inventing information. If the user inquires about lists, format your replies accordingly, whether ordered or unordered.
+
+                If prompted to supply questions derived from the context, furnish genuine questions starting with a capital letter and concluding with a question mark. These questions should not include the corresponding answers.
+
+                Maintain clarity in distinguishing between interviewers (those conducting the interviews) and interviewees (those responding to the questions). Note that there may be multiple interviewers but only one interviewee per interview session.
+              """
+
+
+question_1 = "Who was the interviewee for the CVS interview?"
+question_2 = "What are 10 questions asked during the vanguard interview?"
+question_3 = "Who is Elon Musk?"
+question_4= " Write a short poem about stochastic gradient descent"
+question_5 = "What was the most challenging question during the vanguard interview?"
+question_6 = "Tell me about the performance of the interviewer during the interview?"
+question_7 = "Tell me about 5 questions about the Bayer interview?"
+question_8 = "Based on the how the conversation unfolded during the Bayer interview, would the candidate get the job?"
+question_9 = "What are 10 questions asked during the CVS interview?"
+question_10 = "Can you provide the 10 most challenging questions across all interviews?"
+
+questions = [question_1, question_2, question_3, question_4, question_5, question_6, question_7, question_8, question_9, question_10]
+
